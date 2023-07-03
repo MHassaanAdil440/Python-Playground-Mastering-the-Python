62057ab256d1121bbedf8bba7d1b7bc553b664e1
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-07-03 15:21:57 +0500

    Practicing

---
+++ FirstProgram.py
@@ -0,0 +1,10 @@
+#Little exercise
+
+firstName = 'Hassaan'
+lastName = 'Adil'
+age = 21
+isAdut = True
+MostImpThing = 'Career'
+
+string = 'My Name is '+firstName + lastName + '. I am ' + str(age) + ' years old. Am i and adult: ' + str(isAdut) + ' and the most important thing for me is my ' + MostImpThing
+print(string)
