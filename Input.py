c969bc30eb6365862e652fc9cac5be3c4dd93dde
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-07-03 15:31:21 +0500

    playing with input and typecasting

--- Input.py
+++ Input.py
@@ -1,3 +1,7 @@
-name = input('Enter your name: ')
-age = input('Enter your age: ')
-print('Hello ' + name + ' you are ' + age + ' years old')
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# print('Hello ' + name + ' you are ' + age + ' years old')
+
+oldAge = input('Enter your old age: ')
+newAge = int(oldAge) + 1
+print('Your new age is: ' + str(newAge))
